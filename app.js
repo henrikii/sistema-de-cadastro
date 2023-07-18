5a44b6a53bddcdf00bb6311d1c971135d16e7e09
henrikii <42790184+henrikii@users.noreply.github.com>
2023-07-17 21:55:38 -0300

    Adicionando console log atualização

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("O sistema está rodando o gerencianmento de jogos")
+console.log("O sistema está rodando o gerenciamento de jogos")
+console.log("atualização no sistema de gerenciamento de jogos")
